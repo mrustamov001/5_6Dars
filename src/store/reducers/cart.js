af90cc2648c65517e08dfb2a00fd8996385b3c75
RMuhammadamin001 <muhammadrustamov7009@gmail.com>
2023-02-06 21:37:10 +0500

    Add to product

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -5,6 +5,19 @@ let initialState = {
 
 function cartReducer(state = initialState, action) {
     switch (action.type) {
+        case "ADD_TO_CART": {
+            return {
+                ...state,
+                items: [
+                    ...state.items,
+                    {
+                        product: action.payload,
+                        count: 1,
+                    }
+                ],
+            };
+        }
+
         // case "ADD_TO_CART": {
         //     return {
         //         ...state,
@@ -17,23 +30,6 @@ function cartReducer(state = initialState, action) {
         //         ],
         //     };
         // }
-
-        case "ADD_TO_CART": {
-            return {
-                ...state,
-                items: state.items.map(el => el.product.id === action.payload ? {...el, count: el.count + 1} : {...el, items: [...el.items, {product: action.payload, count: el.count + 1}]})
-            } 
-            // return {
-            //     ...state,
-            //     items: [
-            //         ...state.items,
-            //         {
-            //             product: action.payload,
-            //             count: 1,
-            //         }
-            //     ],
-            // };
-        }
 
         case "INC_ITEM_COUNT": {
             return {
